283c4c43c199770acab9339651082836d181e532
Kyuho Kim <valve.god.project@gmail.com>
2017-04-09 23:55:34 +0900

    make more realistic example

--- dump.js
+++ dump.js
@@ -14,7 +14,8 @@ connection.connect();
 app.get("/data", function(req, res) {
   console.log("param=" + req.query);
 
-  var qstr = 'select * from sensors where time > date_sub(now(), INTERVAL 1 DAY) ';
+  //var qstr = 'select * from sensors where time > date_sub(now(), INTERVAL 1 DAY) ';
+  var qstr = 'select * from sensors ';
   connection.query(qstr, function(err, rows, cols) {
     if (err) {
       throw err;
@@ -24,14 +25,14 @@ app.get("/data", function(req, res) {
 
     console.log("Got "+ rows.length +" records");
     var html = "<!doctype html><html><body>";
-    html += "<H1> Sensor Data for Last 24 Hours</H1>";
-    html += "<table border=1 cellpadding=3 cellspacing=0>";
-    html += "<tr><td>Seq#<td>Time Stamp<td>Temperature";
-    for (var i=0; i< 15 /*rows.length*/; i++) {
-       html += "<tr><td>Seq#<td>Time Stamp<td>Temperature";
+    //html += "<H1> Sensor Data for Last 24 Hours</H1>";
+    //html += "<table border=1 cellpadding=3 cellspacing=0>";
+    //html += "<tr><td>Seq#<td>Time Stamp<td>Temperature";
+    for (var i=0; i< rows.length; i++) {
+       html += "<br>"+ JSON.stringify(rows[i]);
     }
-    html += "</table>";
-    html += "</body></html>";
+    //html += "</table>";
+    //html += "</body></html>";
     res.send(html);
   });
 
